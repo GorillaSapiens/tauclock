c5e846f344f7c0f5e75da229b60b67bb21b3da24
GorillaSapiens <embracetheape@gmail.com>
2022-04-04 14:41:42 -0400

    it compiles i think

--- sunclock1/app/src/main/java/com/wozniakconsulting/sunclock1/MainActivity.kt
+++ sunclock1/app/src/main/java/com/wozniakconsulting/sunclock1/MainActivity.kt
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+      init {
+         System.loadLibrary("sunclock1")
+      }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

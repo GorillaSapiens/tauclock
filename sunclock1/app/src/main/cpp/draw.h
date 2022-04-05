b53f3399a44d567b12d766ae4a8566fe63301387
GorillaSapiens <embracetheape@gmail.com>
2022-04-05 13:50:26 -0400

    fixed colors

--- sunclock1/app/src/main/cpp/draw.h
+++ sunclock1/app/src/main/cpp/draw.h
@@ -41,7 +41,8 @@
 
 #define COLOR_NONE       0x00000000
 
-#define RGB(r,g,b) (0xFF000000 | ((b) << 16) | ((g) << 8) | (r))
+//#define RGB(r,g,b) (0xFF000000 | ((b) << 16) | ((g) << 8) | (r))
+#define RGB(r,g,b) (0xFF000000 | ((r) << 16) | ((g) << 8) | (b))
 
 #define COLOR_BLACK      RGB(  0,   0,   0)
 #define COLOR_BLUE       RGB(  0,   0, 255)

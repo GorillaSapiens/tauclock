c9d64a69ae8fe342ef6b9445764e82f136c78739
GorillaSapiens <embracetheape@gmail.com>
2022-04-05 13:59:05 -0400

    color correction

--- sunclock1/app/src/main/java/com/wozniakconsulting/sunclock1/SunclockDrawable.kt
+++ sunclock1/app/src/main/java/com/wozniakconsulting/sunclock1/SunclockDrawable.kt
@@ -28,6 +28,24 @@ class SunclockDrawable(width: Int, height: Int) : Drawable() {
 
 //                val image = mThing.slice(2 .. mThing.size)
                 canvas.drawBitmap(mThing, 2, w.toInt(), x.toInt(), y.toInt(), w.toInt(), h.toInt(), true, null)
+
+                mPaint.setARGB(255, 0, 0, 0)
+                mPaint.setStrokeWidth(2.0f)
+                mPaint.setStyle(Style.FILL)
+
+                var r : Rect;
+                // above
+                r = Rect(0,0,mWidth.toInt(),y.toInt());
+                canvas.drawRect(r, mPaint);
+                // below
+                r = Rect(0,(y+h).toInt(),mWidth.toInt(), mHeight.toInt());
+                canvas.drawRect(r, mPaint);
+                // left
+                r = Rect(0,y.toInt(),x.toInt(), (y+h).toInt());
+                canvas.drawRect(r, mPaint);
+                // right
+                r = Rect((x+w).toInt(),y.toInt(),mWidth.toInt(), (y+h).toInt());
+                canvas.drawRect(r, mPaint);
             } else {
                 // Set the correct values in the Paint
                 mPaint.setARGB(255, 255, 0, 0)

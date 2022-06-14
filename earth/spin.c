b687daeaf547eb7195708b322151263f451f44bb
GorillaSapiens <embracetheape@gmail.com>
2022-06-14 13:56:13 -0400

    speed optimization

--- earth/spin.c
+++ earth/spin.c
@@ -100,36 +100,45 @@ int main(int argc, char **argv) {
    }
 
    for (int y = 1; y < 255; y++) {
-      for (int x = 1; x < 255; x++) {
-         int d = sqrt((y-128)*(y-128)+(x-128)*(x-128));
-         if (d < 128) {
-            int n = 0;
-            int sr = 0;
-            int sg = 0;
-            int sb = 0;
-            if (page[y][x] == 0) {
-               for (int dy = -1; dy < 2; dy++) {
-                  for (int dx = -1; dx < 2; dx++) {
-                     if (page[y+dy][x+dx] != 0) {
-                        int c = page[y+dy][x+dx];
-                        int r = c >> 16;
-                        int g = (c >> 8) & 0xff;
-                        int b = c & 0xff;
 
-                        n++;
-                        sr += r;
-                        sg += g;
-                        sb += b;
-                     }
+      int yy = y - 128;
+      // 127 = sqrt(yy*yy+xx*xx)
+      // 127*127 - yy*yy = xx*xx
+      int xx;
+      if (yy != 0) {
+         xx = sqrt(127*127 - yy*yy);
+      }
+      else {
+         xx = 127;
+      }
+
+      for (int x = 128-xx; x < 128+xx; x++) {
+         int n = 0;
+         int sr = 0;
+         int sg = 0;
+         int sb = 0;
+         if (page[y][x] == 0) {
+            for (int dy = -1; dy < 2; dy++) {
+               for (int dx = -1; dx < 2; dx++) {
+                  if (page[y+dy][x+dx] != 0) {
+                     int c = page[y+dy][x+dx];
+                     int r = c >> 16;
+                     int g = (c >> 8) & 0xff;
+                     int b = c & 0xff;
+
+                     n++;
+                     sr += r;
+                     sg += g;
+                     sb += b;
                   }
                }
-               if (n != 0) {
-                  sr /= n;
-                  sg /= n;
-                  sb /= n;
-                  int c = (sr << 16) | (sg << 8) | (sb);
-                  page[y][x] = c;
-               }
+            }
+            if (n != 0) {
+               sr /= n;
+               sg /= n;
+               sb /= n;
+               int c = (sr << 16) | (sg << 8) | (sb);
+               page[y][x] = c;
             }
          }
       }

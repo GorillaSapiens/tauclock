6514242ad725364a15d54224be71f78304d309f9
GorillaSapiens <embracetheape@gmail.com>
2023-03-10 20:33:55 -0700

    added settings for light / dark (currently nonfunctional)

--- sunclock1/app/src/main/java/com/gorillasapiens/sunclock1/SettingsActivity.kt
+++ sunclock1/app/src/main/java/com/gorillasapiens/sunclock1/SettingsActivity.kt
@@ -8,13 +8,52 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
-import androidx.preference.EditTextPreference
-import androidx.preference.ListPreference
-import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.*
 import java.util.*
 
-
 class SettingsActivity : AppCompatActivity() {
+
+    val colors: IntArray = intArrayOf(
+        0xFFFFFF00.toInt(), // yellow
+        0xFFFFA500.toInt(), // orange
+        0xFF4CABF4.toInt(), // lt blue
+        0xFF0000FF.toInt(), // blue
+        0xFF000080.toInt() // dk blue
+    )
+
+    fun colorchange(parent: ViewGroup, light: Int, dark: Int, f: Boolean) : Boolean {
+        var second = f;
+        for (i in 0 until parent.getChildCount()) {
+            val child: View = parent.getChildAt(i)
+            if (child is ViewGroup || child is LinearLayout) {
+                second = colorchange(child as ViewGroup, light, dark, second) || second
+                // DO SOMETHING WITH VIEWGROUP, AFTER CHILDREN HAS BEEN LOOPED
+            } else {
+                if (child != null) {
+                    // DO SOMETHING WITH VIEW
+                    if (child is SeekBar) {
+                        if (second) {
+                            // dark
+                            child.getProgressDrawable().setTint(dark)
+                            child.getThumb().setTint(dark)
+                        }
+                        else {
+                            // light
+                            child.getProgressDrawable().setTint(light)
+                            child.getThumb().setTint(light)
+                        }
+                        second = true
+                    }
+                }
+            }
+        }
+        return second
+    }
+
+    fun technicolor(light: Int, dark: Int) {
+        colorchange(window.decorView.rootView as ViewGroup,
+            colors[light], colors[4 - dark], false)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +78,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
@@ -150,9 +190,56 @@ class SettingsActivity : AppCompatActivity() {
                     }
                     true
                 }
-
-
             }
+
+            var light = findPreference<SeekBarPreference>("light")
+            if (light != null) {
+                val light_array = getResources().getStringArray(R.array.light_values);
+                light.setSummary(light_array[light.value])
+                light.setOnPreferenceChangeListener { preference, newValue ->
+                    if (newValue is Int) {
+                        val light_array = getResources().getStringArray(R.array.light_values);
+                        preference.setSummary(light_array[newValue])
+                        val other = findPreference<SeekBarPreference>("dark")
+                        if (other != null) {
+                            val oarray = getResources().getStringArray(R.array.dark_values);
+                            while ((other.value + newValue) > 3) {
+                                val newOther = other.value - 1;
+                                other.value = newOther
+                                other.setSummary(oarray[newOther])
+                            }
+                            val activity = context as SettingsActivity
+                            activity.technicolor(newValue, other.value)
+                        }
+                    }
+                    true
+                }
+            }
+
+            var dark = findPreference<SeekBarPreference>("dark")
+            if (dark != null) {
+                val dark_array = getResources().getStringArray(R.array.dark_values);
+                dark.setSummary(dark_array[dark.value])
+                dark.setOnPreferenceChangeListener { preference, newValue ->
+                    if (newValue is Int) {
+                        val dark_array = getResources().getStringArray(R.array.dark_values);
+                        preference.setSummary(dark_array[newValue])
+                        val other = findPreference<SeekBarPreference>("light")
+                        if (other != null) {
+                            val oarray = getResources().getStringArray(R.array.light_values);
+                            while (((other.value) + newValue) > 3) {
+                                val newOther = other.value - 1;
+                                other.value = newOther
+                                other.setSummary(oarray[newOther])
+                            }
+                            val activity = context as SettingsActivity
+                            activity.technicolor(other.value, newValue)
+                        }
+                    }
+                    true
+                }
+            }
+
         }
     }
 }

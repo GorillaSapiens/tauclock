bc45a97ab2ae29c21fa2f5e53d55ed38ae4480a2
GorillaSapiens <embracetheape@gmail.com>
2023-03-26 02:07:16 -0400

    more comments

--- standalone/draw.c
+++ standalone/draw.c
@@ -1,5 +1,5 @@
 //  Sunclock, draw a clock with local solar and lunar information
-//  Copyright (C) 2022 Adam Wozniak / GorillaSapiens
+//  Copyright (C) 2022,2023 Adam Wozniak / GorillaSapiens
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -13,6 +13,8 @@
 //
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#pragma GCC optimize("Ofast")
 
 #include <math.h>
 #include <stdio.h>

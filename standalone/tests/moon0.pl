3f1dcc859986dd8c044ab6558e658dc9f223cb4a
GorillaSapiens <embracetheape@gmail.com>
2023-11-05 05:36:59 -0500

    da moon, beautiful

--- standalone/tests/moon0.pl
+++ standalone/tests/moon0.pl
@@ -1,19 +1,26 @@
 #!/usr/bin/perl
 
+`rm moon*.png`;
+
 @x = (
 "https://www.timeanddate.com/astronomy/australia/adelaide -34.9285 138.6007",
 "https://www.timeanddate.com/astronomy/italy/rome 41.9028 12.4964",
 "https://www.timeanddate.com/astronomy/antarctica/mcmurdo -77.8500 166.6667",
 "https://www.timeanddate.com/astronomy/usa/honolulu 21.3099 -157.8581",
 "https://www.timeanddate.com/astronomy/canada/tuktoyaktuk 69.4454 -133.0342",
+"https://www.timeanddate.com/moon/usa/chicago 41.8781 -87.62.98"
 );
 
 $i = 0;
 foreach $x (@x) {
    #print "$x\n";
    ($url, $lat, $lon) = split / /, $x;
+
+   @place = split /\//, $url;
+   $place = pop @place;
+
    $cmd = "(wget $url -O - 2>&1 | grep -a moon.php | sed \"s/.*moon.php//g\" | sed \"s/\\\".*//g\")";
-   $cmd2 = "./calcdata $lat $lon -1";
+   $cmd2 = "./calcdata $lat $lon";
    @a = `$cmd`;
    $a[0] =~ s/\&amp;/\&/g;
    $a[0] =~ s/^/https:\/\/www.timeanddate.com\/scripts\/moon.php/g;
@@ -26,10 +33,12 @@ foreach $x (@x) {
 
    `convert -size 1024x1024 -depth 8 RGBA:out.bin out.png`;
 
-   `composite moon.png out.png moon$i.png`;
+   `composite moon.png out.png moon$i\_.png`;
+
+   `convert moon$i\_.png -strokewidth 3 -stroke black -fill white -pointsize 72 -gravity north -font Courier-Bold -draw "text 0,0 $place" moon$i.png`;
 
    $i++;
 }
 
-`montage -border 0 -geometry 1024x moon0.png moon1.png moon2.png moon3.png moon4.png moon0_all.png`;
+`montage -border 0 -geometry 1024x moon0.png moon1.png moon2.png moon3.png moon4.png moon5.png moon0_all.png`;
 `mv moon0_all.png moon_all.png`;
